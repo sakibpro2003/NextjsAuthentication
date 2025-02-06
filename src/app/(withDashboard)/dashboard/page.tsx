1824b56c277cb53dda5d2e25573c9072c8eb37d7
sakibpro2003 <sakibprodhan2003@gmail.com>
2025-02-06 21:48:39 +0600

    github login with next auth

--- src/app/(withDashboard)/dashboard/page.tsx
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -1,7 +1,21 @@
-const DashboardPage = () => {
+import { authOptions } from "@/utils/authOptions";
+import { getServerSession } from "next-auth";
+
+const DashboardPage = async () => {
+  const session = await getServerSession(authOptions);
+  console.log(session);
   return (
     <div>
-      <h1 className="text-4xl text-center mt-10">Welcome To Dashboard Page</h1>
+      <h1 className="text-4xl text-center mt-10">
+        Welcome To {session?.user?.name}
+      </h1>
+      <h1>User email: {session?.user?.email}</h1>
+      {/* <Image
+        src={session?.user?.image}
+        width={100}
+        height={100}
+        alt="user profile picture"
+      /> */}
     </div>
   );
 };
---
+++ src/utils/authOptions.ts
@@ -0,0 +1,14 @@
+import GithubProvider from "next-auth/providers/github";
+
+import { NextAuthOptions } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
